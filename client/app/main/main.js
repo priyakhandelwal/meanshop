01a98e3ae3b94260dbefb803ba974310fa6f3a01
prkhandelwal <prkhandelwal@paypal.com>
2016-03-22 00:55:57 +0530

    first commit

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('meanshopApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        template: '<main></main>'
+      });
+  });
